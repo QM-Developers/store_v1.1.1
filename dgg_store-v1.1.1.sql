492875002b5d657af59c6e34f3707c3c93cc872e
zzgit00 <1109440800@qq.com>
2017-07-11 18:12:21 +0800

    script-commit

--- dgg_store-v1.1.1.sql
+++ dgg_store-v1.1.1.sql
@@ -1441,4 +1441,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-11 18:11:53
+-- Dump completed on 2017-07-11 18:12:20
